525ea263996da761718a78cb1e0680cd16e1ffd3
DouweWiegersma <douwewiegersma17@hotmail.nl>
2026-06-25 14:23:37 +0200

    ClawTreatment Mapper

--- src/main/java/nl/wiegersma/dairyfarm/mappers/ClawDiseaseMapper.java
+++ src/main/java/nl/wiegersma/dairyfarm/mappers/ClawDiseaseMapper.java
@@ -7,15 +7,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-
 public interface ClawDiseaseMapper {
 
     public ClawDiseaseResponseDto toDto(ClawDisease clawDisease);
 
     public ClawDisease toEntity(ClawDiseaseRequestDto clawDiseaseRequestDto);
 
-    public void updateEntity(ClawDiseaseRequestDto clawDiseaseRequestDto, @MappingTarget ClawDisease clawDisease);
+    public void updateClawDisease(ClawDiseaseRequestDto clawDiseaseRequestDto, @MappingTarget ClawDisease clawDisease);
 
+    public List<ClawDisease> clawDiseaseToList(List<ClawDiseaseRequestDto> clawDiseaseRequestDtoList);
+
+    public List<ClawDiseaseResponseDto> clawDiseaseDtoToList(List<ClawDisease> clawDiseaseList);
 }
